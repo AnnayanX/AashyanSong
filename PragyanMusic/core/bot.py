a8c2fd01f208a855203a1f63df37145e41c49b62
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-10-17 21:00:44 +0530

    Update bot.py

--- PragyanMusic/core/bot.py
+++ PragyanMusic/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class PragyanMusic(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Starting Bot... | Logger: @pragyanmusiclogs")
         super().__init__(
             name="PragyanMusic",
             api_id=config.API_ID,
@@ -26,28 +26,35 @@ class PragyanMusic(Client):
         self.mention = self.me.mention
 
         try:
+            # Get the chat by username
+            log_chat = await self.get_chat("@pragyanmusiclogs")
+            
+            # Send a message to the log channel
             await self.send_message(
-                chat_id=config.LOGGER_ID,
+                chat_id=log_chat.id,
                 text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel. | Logger: @pragyanmusiclogs"
             )
             exit()
         except Exception as ex:
             LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}. | Logger: @pragyanmusiclogs"
             )
             exit()
 
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+        # Ensure the bot is an admin in the log group
+        a = await self.get_chat_member(log_chat.id, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
+                "Please promote your bot as an admin in your log group/channel. | Logger: @pragyanmusiclogs"
             )
             exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+
+        LOGGER(__name__).info(f"Music Bot Started as {self.name} | Logger: @pragyanmusiclogs")
 
     async def stop(self):
         await super().stop()
+        LOGGER(__name__).info(f"Music Bot Stopped. | Logger: @pragyanmusiclogs")
