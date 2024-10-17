30dbead7fbee4913e47a9edea50e27dc9958d537
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-10-17 21:48:01 +0530

    Update userbot.py

--- PragyanMusic/core/userbot.py
+++ PragyanMusic/core/userbot.py
@@ -11,7 +11,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="PragyanMusic1",
+            name="PragyanMusicAssistant",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
